a02d1bc8a59d694415273a237b93cbeaacb534f9
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-03-16 14:11:46 +0530

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,4 +1,18 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
-        
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         return str(x) == str(x)[::-1]
+
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        original=x
+        reverse=0
+        while x!=0:
+            digit =x%10
+            reverse = reverse*10 +digit
+            x=x//10
+        return original == reverse
+

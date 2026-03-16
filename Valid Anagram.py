0b73b17a35db9db7581d073f3a69b9898e178a4d
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-03-16 16:58:44 +0530

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -9,7 +9,32 @@ class Solution:
             tkey=ord(t[i])-ord("a")
             newdict[skey]+=1
             newdict[tkey]-=1
-        for i in range(26):
-            if newdict[i]!=0:
+        for j in range(26):
+            if newdict[j]!=0:
                 return False
+        return True
+
+
+#method 2
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        newdict ={}
+        for i in s:
+            newdict[i]=newdict.get(i,0)+1
+        
+        for j in t:
+            if j not in newdict:
+                return False
+            newdict[j]-=1
+
+            if newdict[j]<0:
+                return false
+
         return True

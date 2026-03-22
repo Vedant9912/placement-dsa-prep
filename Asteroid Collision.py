251c4798f3b53bf347fcb5c9072b92955da9a6a3
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-03-23 01:17:56 +0530

    Create Asteroid Collision.py

---
+++ Asteroid Collision.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack=[]
+        for i in asteroids:
+            while stack and i<0 and stack[-1]>0:
+                if stack[-1]< -i:
+                    stack.pop()
+                elif stack[-1]== -i:
+                    stack.pop()
+                    break
+                else:
+                    break
+
+            else:
+                stack.append(i)
+        return stack

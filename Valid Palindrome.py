16d81c37e829f9a93160efd1d4461e329dfe1cd5
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-04-25 17:28:00 +0530

    Valid Palindrome.py
    Removed extraneous Java code comments from the Python file.

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -5,3 +5,19 @@ class Solution:
             if i.isalnum():
                 newS+=i.lower()
         return newS==newS[::-1]
+
+
+# JAVA
+# class Solution {
+#     public boolean isAnagram(String s, String t) {
+#         char[] arr1=s.toCharArray();
+#         char[] arr2=t.toCharArray();
+#         Arrays.sort(arr1);
+#         Arrays.sort(arr2);
+#         return Arrays.equals(arr1,arr2);
+        
+        
+
+        
+#     }
+# }

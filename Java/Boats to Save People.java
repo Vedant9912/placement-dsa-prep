99f12973ec24f80dbf8939f057d54742a35b1692
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-05-09 16:52:59 +0530

    Implement rescue boat allocation algorithm

---
+++ Java/Boats to Save People.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count=0;
+        int left= 0;
+        int right=people.length-1;
+        while ( left <= right){
+            if (people[left]+people[right]<=limit){
+                count++;
+                left++;
+                right--;
+            }
+            else{
+                count++;
+                right--;
+            }
+
+
+        }
+        return count;
+        
+    }
+}

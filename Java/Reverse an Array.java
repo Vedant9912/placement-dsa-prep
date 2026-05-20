c2e3cd6c20691b54cf866e478d1bb10d3afa1b7c
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-05-20 17:02:00 +0530

    Add method to find largest element in array
    Implement method to find the largest element in an array.

---
+++ Java/Reverse an Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+        int x=arr[0];
+        for(int i=1 ; i < arr.length ; i++){
+            if ( x<arr[i]){
+                x=arr[i];
+            }
+        }
+        return x;
+        
+    }
+}
+

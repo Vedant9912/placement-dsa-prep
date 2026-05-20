e05496cbc142d2a069a893c89062ea4400f59dda
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-05-20 17:11:57 +0530

    Implement array sorted check method
    Added a method to check if an array is sorted in ascending order.

--- Java/Check if array is sorted.java
+++ Java/Check if array is sorted.java
@@ -10,3 +10,17 @@ class Solution {
         return false;
     }
 }
+
+
+
+import java.util.Arrays;
+class Solution {
+    public boolean isSorted(int[] arr) {
+        for ( int i = 1 ; i < arr.length  ; i++){
+            if (arr[i-1]>arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}

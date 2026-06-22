e673c9a5832d8af302e0b23a957941288e1f85e3
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-06-23 01:46:30 +0530

    Create Maximum Number of Balloons.java

---
+++ Java/Maximum Number of Balloons.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] arr= new int[5];
+        for(int i=0 ; i < text.length() ; i++ ){
+            switch(text.charAt(i)){
+                case 'b' :
+                      arr[0]++;
+                      break;
+                case 'a' :
+                      arr[1]++;
+                      break;
+                case 'l' :
+                      arr[2]++;
+                      break;
+                case 'o' :
+                      arr[3]++;
+                      break;
+                case 'n' :
+                      arr[4]++;
+                      break;
+            }
+        }
+        arr[2]=arr[2]/2;
+        arr[3]=arr[3]/2;
+        int smallest=arr[0];
+        for(int i = 0 ; i < arr.length ; i++){
+            if (arr[i]<smallest){
+                smallest=arr[i];
+            }
+
+        }
+        return smallest;
+        
+    }
+}

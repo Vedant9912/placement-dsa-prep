2cf894a319c7954ca278771f80467ae9e6e305fe
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-06-29 13:51:17 +0530

    Add solution to find duplicates in an array
    Implement a method to find all duplicates in an array.

---
+++ Java/Find All Duplicates in an Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> arr= new ArrayList<>();
+        for(int i=0; i< nums.length ; i++){
+            int idx=Math.abs(nums[i])-1;
+            if(nums[idx]<0){
+                arr.add(idx+1);
+            }
+            else{
+                nums[idx]=-nums[idx];
+            }
+        }
+        return arr;
+
+        
+    }
+}

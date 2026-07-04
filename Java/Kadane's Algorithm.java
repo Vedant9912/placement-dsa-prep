0788c23addf157632b5e9ec64d0896fb245399eb
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-07-04 17:37:43 +0530

    Implement Kadane's Algorithm for max subarray sum

---
+++ Java/Kadane's Algorithm.java
@@ -0,0 +1,13 @@
+class Solution {
+	int maxSubarraySum(int[] arr) {
+		// Code here
+		int max=arr[0];
+		int cur=arr[0];
+		for(int i = 1 ; i < arr.length ; i++){
+		    cur= Math.max(arr[i], cur+arr[i]);
+		    max=Math.max(cur,max);
+		}
+		return max;
+		
+	}
+}

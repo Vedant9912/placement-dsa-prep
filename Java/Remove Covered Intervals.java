d280ad0e8ccc5f7361151e089ab7f88dc2d11a16
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-07-07 07:52:01 +0530

    Add solution for removing covered intervals
    Implement a solution to remove covered intervals from a list of intervals.

---
+++ Java/Remove Covered Intervals.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> {
+            if (a[0] == b[0]){
+                return Integer.compare(b[1],a[1]);
+        }
+           return Integer.compare(a[0],b[0]);
+        });
+        int count=0;
+        int[] pair = new int[]{intervals[0][0],intervals[0][1]};
+        for(int i = 1 ; i < intervals.length ; i++){
+            if(intervals[i][0]>=pair[0]){
+                if(intervals[i][1]<=pair[1]){
+
+                    count++;
+
+                }
+                else{
+                pair[0]=intervals[i][0];
+                pair[1]=intervals[i][1];
+            }
+            }
+            else{
+                pair[0]=intervals[i][0];
+                pair[1]=intervals[i][1];
+            }
+
+
+        }
+        return intervals.length-count;
+
+        
+    }
+}
